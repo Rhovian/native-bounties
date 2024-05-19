7049c523520491c98484b9a85509400b831e4c15
rhovian <atomicjab@proton.me>
2024-05-18 21:00:15 -0500

    refactor tests

--- tests/close_bounty.rs
+++ tests/close_bounty.rs
@@ -5,14 +5,8 @@ mod tests {
     use native_mutual_assurance::{
         instruction::{CreateBountyArgs, PoidhInstruction},
         processor::process_instruction,
-        state::Bounty,
     };
-    use solana_program::{
-        instruction::{AccountMeta, Instruction},
-        program_pack::Pack,
-        pubkey::Pubkey,
-        rent::Rent,
-    };
+    use solana_program::instruction::{AccountMeta, Instruction};
     use solana_program_test::*;
     use solana_sdk::{
         signature::{Keypair, Signer},
@@ -20,16 +14,15 @@ mod tests {
     };
 
     #[tokio::test]
-    async fn test_create_bounty() {
+    async fn test_close_bounty() {
         let program_test = ProgramTest::new("Poidh", ID, processor!(process_instruction));
-
         let mint = Keypair::new();
-
         let (mut banks_client, payer, recent_blockhash) = program_test.start().await;
 
         let (bounty_account, _) = find_bounty_account(&payer.pubkey(), &mint.pubkey());
 
-        let mut transaction = Transaction::new_with_payer(
+        // Create the bounty
+        let mut create_transaction = Transaction::new_with_payer(
             &[Instruction::new_with_borsh(
                 ID,
                 &PoidhInstruction::CreateBounty(CreateBountyArgs {
@@ -46,32 +39,37 @@ mod tests {
             )],
             Some(&payer.pubkey()),
         );
+        create_transaction.sign(&[&payer], recent_blockhash);
+        banks_client
+            .process_transaction(create_transaction)
+            .await
+            .unwrap();
 
-        transaction.sign(&[&payer], recent_blockhash);
+        // Close the bounty
+        let mut close_transaction = Transaction::new_with_payer(
+            &[Instruction::new_with_borsh(
+                ID,
+                &PoidhInstruction::CloseBounty,
+                vec![
+                    AccountMeta::new(payer.pubkey(), true),
+                    AccountMeta::new(bounty_account, false),
+                    AccountMeta::new_readonly(mint.pubkey(), false),
+                    AccountMeta::new_readonly(solana_program::system_program::id(), false),
+                ],
+            )],
+            Some(&payer.pubkey()),
+        );
+        close_transaction.sign(&[&payer], recent_blockhash);
+        banks_client
+            .process_transaction(close_transaction)
+            .await
+            .unwrap();
 
-        banks_client.process_transaction(transaction).await.unwrap();
-
-        let bounty_account_info = banks_client
+        // Check that the bounty account is closed
+        assert!(banks_client
             .get_account(bounty_account)
             .await
-            .expect("get_account")
-            .expect("bounty_account not found");
-
-        assert_eq!(bounty_account_info.owner, ID);
-        assert_eq!(bounty_account_info.data.len(), Bounty::LEN);
-        assert_eq!(
-            bounty_account_info.lamports,
-            Rent::default().minimum_balance(Bounty::LEN) + 100_000_000
-        );
-
-        let bounty = Bounty::unpack_from_slice(&bounty_account_info.data).unwrap();
-        assert!(bounty.is_initialized);
-        assert_eq!(bounty.owner, payer.pubkey());
-        assert_eq!(bounty.mint, mint.pubkey());
-        assert_eq!(bounty.name, "Test Bounty");
-        assert_eq!(bounty.description, "A test bounty");
-        assert_eq!(bounty.amount, 100_000_000);
-        assert_eq!(bounty.claimer, Pubkey::default());
-        assert_eq!(bounty.claim_id, 0);
+            .unwrap()
+            .is_none());
     }
 }
